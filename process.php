4bd4123841a9e2da365c556bfd91c1123be260bb
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-10-27 15:50:30 +0300

    Update process.php

--- process.php
+++ process.php
@@ -1,5 +1,9 @@
 <?php 
+	session_start();
+	$name = '';
+	$location = '';
 	$conn = mysqli_connect('localhost','root','','crud');
+	
 	
 	if (isset($_POST['save'])){
 		$name = $_POST['name'];
@@ -8,5 +12,35 @@
 		$sql = "INSERT INTO locations (name,location) VALUES ('$name','$location')";
 		mysqli_query($conn,$sql);
 
+		$_SESSION['message'] = "Record saved";
+		$_SESSION['msg_type'] = "success";
+
+		header("location: index.php");
+
+	}
+
+	if(isset($_GET['delete'])){
+		$id = $_GET['delete'];
+
+		$delquery = "DELETE FROM locations WHERE id = $id";
+		mysqli_query($conn, $delquery);
+
+		$_SESSION['message'] = "Record deleted";
+		$_SESSION['msg_type'] = 'danger';
+
+		header("location: index.php");
+	}
+
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$query = "SELECT * FROM locations WHERE id=$id";
+
+		$result = mysqli_query($conn, $query);
+
+		if(count(mysqli_fetch_assoc($result))==1){
+			$row = mysqli_fetch_array($result);
+			$name = $row['name'];
+			$location =$row['location'];
+		}
 	}
 ?>
